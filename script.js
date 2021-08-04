7c50e50f80af6408a4b521af493244dd0756101d
Ekwugha <elloekwugha@gmail.com>
2021-08-04 13:17:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// fixed navbar
+const navbar = document.querySelector('.navbar');
+// const a = navbar.style.backgroundColor = 'white';
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.pageYOffset;
+    // const height = window.pageXOffset().height;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-top');
+        navbar.style.backgroundColor = 'white';
+        navbar.style.color = 'black';
+    } else {
+    navbar.classList.remove('fixed-top');
+    navbar.style.color = 'white';
+    } 
+})
+
+// set the date at the footer dynamically
+const newDate = document.querySelector('.date');
+newDate.innerHTML = new Date().getFullYear();
